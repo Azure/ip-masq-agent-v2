e27baad145a68f6114621fdbba8cb4840e1d851a
mattstam <mattstam@microsoft.com>
2021-12-10 09:03:26 -0800

    Update to the current thockin/go-build-template

--- build/build.sh
+++ build/build.sh
@@ -14,27 +14,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -o errexit
 set -o nounset
 set -o pipefail
 
-if [ -z "${PKG}" ]; then
-    echo "PKG must be set"
+if [ -z "${OS:-}" ]; then
+    echo "OS must be set"
     exit 1
 fi
-if [ -z "${ARCH}" ]; then
+if [ -z "${ARCH:-}" ]; then
     echo "ARCH must be set"
     exit 1
 fi
-if [ -z "${VERSION}" ]; then
+if [ -z "${VERSION:-}" ]; then
     echo "VERSION must be set"
     exit 1
 fi
 
 export CGO_ENABLED=0
 export GOARCH="${ARCH}"
+export GOOS="${OS}"
+export GO111MODULE=on
+export GOFLAGS="${GOFLAGS:-} -mod=${MOD}"
 
-go install                                                         \
-    -installsuffix "static"                                        \
-    -ldflags "-s -w -X ${PKG}/pkg/version.VERSION=${VERSION}"            \
-    ./...
+go install                                                      \
+    -installsuffix "static"                                     \
+    -ldflags "-X $(go list -m)/pkg/version.Version=${VERSION}"  \
+    "$@"
